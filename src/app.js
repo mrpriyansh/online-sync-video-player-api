b2a820f7f943e73cfb6f7dd3b101b7482d127824
Gaharana <mr.priyanshgaharana@gmail.com>
2020-06-23 19:39:37 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const config = require('./utils/config');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api', require('./api/index.js'));
+
+app.get('*', (req, res) => {
+  res.status(404).send('You did something wrong!');
+});
+
+const port = config.port || 4000;
+
+app.listen(port, () => console.log(`API server ready at http://localhost:${port}`));
+
+
