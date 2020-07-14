b274800c92e9874f2f029bd011dc70474c016b57
Siddharth <siddharth.verman@gmail.com>
2020-07-14 12:50:35 +0530

    Added Login Route

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => res.send('Server Up and Running'));
+app.use('/login', require('./routes/api/login'));
 
 const PORT = process.env.port || 4000;
 app.listen(PORT, () => console.log(`Server Running at port ${PORT}`));
