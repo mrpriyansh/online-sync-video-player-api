9b0a52d52e8cea8689563a4806837580acad9f49
GokulRaj-R <gokul8262@gmail.com>
2020-07-14 15:27:09 +0530

    Add register route

---
+++ routes/api/register.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+
+const router = express.Router();
+const User = require('../../models/User');
+
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Please enter a password with atleast 6 characters').isLenght({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (user) return res.status(400).json({ msg: 'User already exists' });
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(payload, config.get('secretKey'), { expiresIn: '5 days' }, (err, token) => {
+        if (err) throw err;
+        return res.json({ token });
+      });
+      return res.json('Unexpected error happened');
+    } catch (err) {
+      return res.status(400).json({ msg: 'User already exists' });
+    }
+  }
+);
+
+module.exports = router;
