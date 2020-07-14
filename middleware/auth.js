6255d433e20d7c633a9214482577a1f26e881659
Utkarsh Agrawal <uagrawal29.ua@gmail.com>
2020-07-14 18:03:34 +0530

    Add auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function(req, res, next) {
+  // Get token from header
+  const token = req.header('x-auth-token');
+
+  // Check if not token
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
+
+  // Verify token
+  try {
+    jwt.verify(token, config.get('jwtSecret'), (error, decoded) => {
+      if (error) {
+        return res.status(401).json({ msg: 'Token is not valid' });
+      }
+
+      req.user = decoded.user;
+      next();
+
+      return res.json('123');
+    });
+  } catch (err) {
+    console.error('something wrong with auth middleware');
+    res.status(500).json({ msg: 'Server Error' });
+  }
+  return res.json('123');
+};
