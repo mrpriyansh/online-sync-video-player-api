0172800a65044d92a229b40ab22837839b6725e9
Gokul Raj <43962254+GokulRaj-R@users.noreply.github.com>
2020-07-14 16:04:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ connectDB();
 app.get('/', (req, res) => res.send('Server Up and Running'));
 app.use('/login', require('./routes/api/login'));
 app.use('/register', require('./routes/api/register'));
-app.user('/contact', require('./routes/api/contactUs'));
+app.use('/contact', require('./routes/api/contactUs'));
 
 const PORT = process.env.port || 4000;
 app.listen(PORT);
