f466b01bc489f4c21ea678807340a761fba41bcb
Utkarsh Agrawal <uagrawal29.ua@gmail.com>
2020-07-14 17:33:49 +0530

    Add Contact Schema

---
+++ models/ContactUs.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const Contact = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Contact', Contact);
