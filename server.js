efada3241d66f427ad68d038e62a147775891b28
Anuj Singh <anujs787@gmail.com>
2020-07-21 20:30:51 +0530

    .

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ io.on('connection', socket => {
 app.get('/', (req, res) => res.send('Server Up and Running'));
 app.use('/login', require('./routes/api/login'));
 app.use('/register', require('./routes/api/register'));
-app.use('/contact', require('./routes/api/contactus'));
+app.use('/contact', require('./routes/api/contactUs'));
 
 const PORT = process.env.port || 4000;
 app.listen(PORT, () => console.log(`Server is up on port ${PORT}`));
