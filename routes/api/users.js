171003283bd6805202aeee6888d806fc61c35911
Siddharth <siddharth.verman@gmail.com>
2020-07-21 16:14:55 +0530

    Added socket.io / Update User Schema

---
+++ routes/api/users.js
@@ -0,0 +1,28 @@
+const User = require('../../models/User.js');
+
+const addUser = (socketId, name, room) => {
+  // const existingUser = Users.find((user) => user.name === name && user.room === room)
+
+  // if(existingUser) return(error: What error???????????? )
+  const user = User.findOneAndUpdate({ name }, { $set: { socketId, room } }, { new: true });
+
+  return user;
+};
+
+const getUser = socketId => {
+  const user = User.findOne({ socketId });
+
+  return user;
+};
+
+const removeUser = socketId => {
+  const user = User.findOneAndUpdate({ socketId }, { $set: { socketId: '', room: '' } });
+  return user;
+};
+
+const getUserInRoom = room => {
+  const users = User.find({ room });
+  return users;
+};
+
+module.exports = { addUser, getUser, removeUser, getUserInRoom };
