a4430db72c2f283aa46ac5b15c414032697865bf
GokulRaj-R <gokul8262@gmail.com>
2020-07-23 20:11:21 +0530

    Update socket configuration, Change user schema

--- controllers/socket.js
+++ controllers/socket.js
@@ -1,30 +1,32 @@
 const express = require('express');
 
 const router = express.Router();
-const { addUser, getUser, removeUser, getUsersInRoom } = require('./users');
+const { addUser, getUser, removeUser } = require('./users');
 
 module.exports = function(io) {
-  io.on('connection', socket => {
-    socket.on('join', ({ name, room }, callback) => {
-      const { error, user } = addUser({ socketId: socket.id, name, room });
-      if (error) return callback(error);
-
+  io.on('connect', socket => {
+    socket.on('join', async ({ userId, room }, callback) => {
+      const { err, user } = await addUser(socket.id, userId, room);
+      if (err) callback(err);
       socket.join(user.room);
-      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+      socket.emit('message', { user: 'admin', text: 'Welcome to room' });
+      socket.broadcast
+        .to(user.room)
+        .emit('message', { user: 'admin', text: `${user.name} joined the chat` });
       return callback();
     });
 
-    socket.on('sendMessage', (message, callback) => {
-      const user = getUser(socket.id);
+    socket.on('sendMessage', async (message, callback) => {
+      const { user } = await getUser(socket.id);
       io.to(user.room).emit('message', { user: user.name, text: message });
       return callback();
     });
 
-    socket.on('disconnect', () => {
-      const user = removeUser(socket.id);
+    socket.on('disconnect', async () => {
+      const { user } = await removeUser(socket.id);
       if (user) {
         io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
-        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+        // io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
       }
     });
   });
