29580558bb054b8f0518f4adf6a4d37a0374816d
Gokul Raj <43962254+GokulRaj-R@users.noreply.github.com>
2020-07-24 14:12:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const socketio = require('socket.io');
-const http = require('https');
+const http = require('http');
 const cors = require('cors');
 const connectDB = require('./config/db');
 
